06e872de367443d1bdb2745af0b22e2e6310022a
The Buildbot <buildbot@angular2buch.de>
2017-01-14 00:17:28 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/angular-buch/book-monkey2/commit/63d46028394abf1b5c5cf3277e7bdf926fbb1a06
    Travis build: https://travis-ci.org/angular-buch/book-monkey2/builds/191809057

---
+++ main.bundle.js
@@ -0,0 +1,19 @@
+webpackJsonp([1,2],{
+
+/***/ 10:
+/***/ function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(4);
+
+
+/***/ },
+
+/***/ 4:
+/***/ function(module, exports) {
+
+throw new Error("Module build failed: TypeError: Cannot read property 'newLine' of undefined\n    at Object.getNewLineCharacter (/home/travis/build/angular-buch/book-monkey2/node_modules/typescript/lib/typescript.js:8062:20)\n    at Object.createCompilerHost (/home/travis/build/angular-buch/book-monkey2/node_modules/typescript/lib/typescript.js:44978:26)\n    at Object.ngcLoader (/home/travis/build/angular-buch/book-monkey2/node_modules/@ngtools/webpack/src/loader.js:171:31)");
+
+/***/ }
+
+},[10]);
+//# sourceMappingURL=main.bundle.map
